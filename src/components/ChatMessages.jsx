090e8a4cfaf027ca850f8a2a5346a43b4ea4897c
nanishat <spy.spid3y@gmail.com>
2025-10-09 01:12:32 +0600

    created auto scroll feature

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
 import './ChatMessages.css';
 
 function ChatMessages({ chatMessages }) {
+
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
   return (
     <div
       className="chat-messages-container"
+      ref={chatMessagesRef}
     >
       {chatMessages.map((chatMessage) => {
         return (
